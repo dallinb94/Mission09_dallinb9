e48323b5cf050803b76d75ecd6e1436aafd7a83f
Dallin Blanc <dallinb94@gmail.com>
2023-03-01 23:46:26 -0700

    Add project files.

---
+++ Mission09_dallinb9/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_dallinb9.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_dallinb9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+            
+            var data = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize);
+
+            return View(data);
+        }
+    }
+}
---
+++ Mission09_dallinb9/Models/EFBookstoreRespository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_dallinb9.Models
+{
+    public class EFBookstoreRespository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRespository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
